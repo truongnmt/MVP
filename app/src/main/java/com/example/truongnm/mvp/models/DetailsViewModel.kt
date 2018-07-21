3da6a3202459b4f80cc5198e6ebd58f300457865
TruongNM <manhtruonghedspi@gmail.com>
2018-07-21 18:26:32 +0700

    Set up base packages, create api requests and data classes

---
+++ app/src/main/java/com/example/truongnm/mvp/models/DetailsViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.truongnm.mvp.models
+
+import com.google.gson.Gson
+
+data class DetailsViewModel(val posts: List<Post>, val users: List<User>, val albums: List<Album>) {
+    fun toJson(): String {
+        return Gson().toJson(this)
+    }
+}
---
+++ app/src/main/java/com/example/truongnm/mvp/models/Post.kt
@@ -0,0 +1,3 @@
+package com.example.truongnm.mvp.models
+
+data class Post(val id: Int, val userId: Int, val title: String, val body: String)
